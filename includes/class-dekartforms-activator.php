20550aa90b2e4859b22f161ab5f6eda6ccaa1a2b
Aram Dekart <aram.dekart@gmail.com>
2017-05-04 14:27:07 +0400

    Add a functionality for adding necessary tables into database on plugin activation

--- includes/class-dekartforms-activator.php
+++ includes/class-dekartforms-activator.php
@@ -30,7 +30,97 @@ class Dekartforms_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		
+		// Add necessary tables into database on activation
+		Dekartforms_Activator::add_necessary_tables();
+	}
+	
+	public static function add_necessary_tables() {
+		global $table_prefix, $wpdb;
 
+		require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+		
+		$tblname = 'dekart_forms';
+		$wp_track_table = $table_prefix . "$tblname ";
+		
+		if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+		{
+		
+			$sql = "CREATE TABLE ". $wp_track_table . " ( ";
+			$sql .= "  id  int(11)   NOT NULL auto_increment, ";
+			$sql .= "  title  varchar(255)   NOT NULL, ";
+			$sql .= "  PRIMARY KEY  `id`  (`id`) "; 
+			$sql .= ") ENGINE=InnoDB " . $charset_collate . " AUTO_INCREMENT=1 ; ";
+			
+			$res = dbDelta($sql);
+	
+		}
+
+		$tblname = 'dekart_fields';
+		$wp_track_table = $table_prefix . "$tblname ";
+		
+		if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+		{
+		
+			$sql = "CREATE TABLE ". $wp_track_table . " ( ";
+			$sql .= "  id  int(11)   NOT NULL auto_increment, ";
+			$sql .= "  form_id  int(11)   NOT NULL, ";
+			$sql .= "  name  varchar(255)   NOT NULL, ";
+			$sql .= "  type  varchar(255)   NOT NULL, ";
+			$sql .= "  PRIMARY KEY  id  (id),  "; 
+			$sql .= "  FOREIGN KEY  (form_id)  REFERENCES  " . $table_prefix . "dekart_forms  (id) ";
+			$sql .= "  ON DELETE CASCADE  ";
+			$sql .= "  ON UPDATE CASCADE  ";
+			$sql .= ") ENGINE=InnoDB " . $charset_collate . " AUTO_INCREMENT=1 ; ";
+			
+			$res = dbDelta($sql);
+	
+		}	
+		
+		$tblname = 'dekart_entries';
+		$wp_track_table = $table_prefix . "$tblname ";
+		
+		if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+		{
+		
+			$sql = "CREATE TABLE ". $wp_track_table . " ( ";
+			$sql .= "  id  int(11)   NOT NULL auto_increment, ";
+			$sql .= "  form_id  int(11)   NOT NULL, ";
+			$sql .= "  submitted  TIMESTAMP   DEFAULT  CURRENT_TIMESTAMP, ";
+			$sql .= "  PRIMARY KEY  id  (id),  "; 
+			$sql .= "  FOREIGN KEY  (form_id)  REFERENCES  " . $table_prefix . "dekart_forms  (id) ";
+			$sql .= "  ON DELETE CASCADE  ";
+			$sql .= "  ON UPDATE CASCADE  ";
+			$sql .= ") ENGINE=InnoDB " . $charset_collate . " AUTO_INCREMENT=1 ; ";
+			
+			$res = dbDelta($sql);
+	
+		}
+
+		$tblname = 'dekart_entries_fields';
+		$wp_track_table = $table_prefix . "$tblname ";
+		
+		if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+		{
+		
+			$sql = "CREATE TABLE ". $wp_track_table . " ( ";
+			$sql .= "  id  int(11)   NOT NULL auto_increment, ";
+			$sql .= "  field_id  int(11)   NOT NULL, ";
+			$sql .= "  entry_id  int(11)   NOT NULL, ";
+			$sql .= "  content  text  NOT NULL, ";
+			$sql .= "  PRIMARY KEY  id  (id),  "; 
+			$sql .= "  FOREIGN KEY  (field_id)  REFERENCES  " . $table_prefix . "dekart_fields  (id) ";
+			$sql .= "  ON DELETE CASCADE  ";
+			$sql .= "  ON UPDATE CASCADE,  ";			
+			$sql .= "  FOREIGN KEY  (entry_id)  REFERENCES  " . $table_prefix . "dekart_entries  (id) ";
+			$sql .= "  ON DELETE CASCADE  ";
+			$sql .= "  ON UPDATE CASCADE  ";
+			$sql .= ") ENGINE=InnoDB " . $charset_collate . " AUTO_INCREMENT=1 ; ";
+			
+			$res = dbDelta($sql);
+	
+		}			
+				
 	}
 
 }
