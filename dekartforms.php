4e8cf79202b01974b5abf3deae2a7eaea6af306c
Aram Dekart <aram.dekart@gmail.com>
2017-05-04 12:22:11 +0400

    Create a plugin

---
+++ dekartforms.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Dekart Forms
+Plugin URI: 
+Description: Create forms and put it into your posts/pages
+Author: Aram Khachikyan
+Version: 1.0
+Author URI: http://aramkhachikyan.com
+*/
